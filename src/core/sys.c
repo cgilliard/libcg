9f99b35c9261074b64ada4c6ed9f55bcdf14f55d
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-20 21:05:20 -0700

    address void args

--- src/core/sys.c
+++ src/core/sys.c
@@ -32,19 +32,26 @@
 		"    ret\n");
 #endif
 
-#define IMPL_WRAPPER(ret_type, name, ...)                     \
-	ret_type v = syscall_##name(__VA_OPT__(__VA_ARGS__)); \
-	if (v < 0) {                                          \
-		err = -v;                                     \
-		return -1;                                    \
-	}                                                     \
+#define IMPL_WRAPPER(ret_type, name, ...)         \
+	ret_type v = syscall_##name(__VA_ARGS__); \
+	if (v < 0) {                              \
+		err = -v;                         \
+		return -1;                        \
+	}                                         \
 	return v;
 
 DECLARE_SYSCALL(int, sched_yield, 24, 158, void)
 
 DECLARE_SYSCALL(ssize_t, write, 1, 4, int fd, void *buf, size_t length)
 
-int sched_yield(void){IMPL_WRAPPER(int, sched_yield)}
+int sched_yield(void) {
+	int v = syscall_sched_yield();
+	if (v < 0) {
+		err = -v;
+		return -1;
+	}
+	return v;
+}
 
 ssize_t write(int fd, void *buf, size_t length) {
 	IMPL_WRAPPER(ssize_t, write, fd, buf, length)
