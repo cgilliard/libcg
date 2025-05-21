a828f7e5593a5a95b52bf14df36cc79a963bc702
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-20 20:27:41 -0700

    write

--- src/core/sys.c
+++ src/core/sys.c
@@ -32,3 +32,33 @@ int sched_yield(void) {
 	return v;
 }
 
+ssize_t syscall_write(int fd, void *buf, size_t length);
+#ifdef __linux__
+__asm__(
+    "_syscall_write:\n"
+    "    movq $1, %rax\n"
+    "    syscall\n"
+    "    ret\n");
+#elif defined(__APPLE__)
+__asm__(
+    "_syscall_write:\n"
+    "    sub sp, sp, #16\n"
+    "    str x30, [sp]\n"
+    "    mov x16, #4\n"
+    "    orr x16, x16, #0x2000000\n"
+    "    svc #0x80\n"
+    "    b.cc 1f\n"
+    "    neg x0, x0\n"
+    "1:  ldr x30, [sp]\n"
+    "    add sp, sp, #16\n"
+    "    ret\n");
+#endif /* __APPLE__ */
+
+ssize_t write(int fd, void *buf, size_t length) {
+	ssize_t v = syscall_write(fd, buf, length);
+	if (v < 0) {
+		err = -v;
+		return -1;
+	}
+	return v;
+}
