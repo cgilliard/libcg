3c577ed82f1e069fc6e688d73f63b07c7a10fd88
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-20 20:24:29 -0700

    sched_yield

--- src/core/sys.c
+++ src/core/sys.c
@@ -1,47 +1,27 @@
 #include <error.h>
 #include <types.h>
 
+int syscall_sched_yield(void);
 #ifdef __linux__
-long syscall_wrapper(long id, ...);
 __asm__(
-    "syscall_wrapper:\n"
-    "    movq %rdi, %rax\n"
-    "    movq %rsi, %rdi\n"
-    "    movq %rdx, %rsi\n"
-    "    movq %rcx, %rdx\n"
-    "    movq %r8, %r10\n"
-    "    movq %r9, %r8\n"
-    "    movq 8(%rsp), %r9\n"
+    "_syscall_sched_yield:\n"
+    "    movq $24, %rax\n"
     "    syscall\n"
     "    ret\n");
-#endif
-
-#ifdef __linux__
-#define DECLARE_SYSCALL(ret_type, name, linux_num, macos_num, ...) \
-	ret_type syscall_##name(__VA_ARGS__) {                     \
-		return syscall_wrapper(linux_num);                 \
-	}
 #elif defined(__APPLE__)
-#define DECLARE_SYSCALL(ret_type, name, linux_num, macos_num, ...) \
-	ret_type syscall_##name(__VA_ARGS__);                      \
-	__asm__(".global _syscall_" #name                          \
-		"\n"                                               \
-		"_syscall_" #name                                  \
-		":\n"                                              \
-		"    sub sp, sp, #32\n"                            \
-		"    str x30, [sp]\n"                              \
-		"    mov x16, #" #macos_num                        \
-		"\n"                                               \
-		"    orr x16, x16, #0x2000000\n"                   \
-		"    svc #0x80\n"                                  \
-		"    b.cc 1f\n"                                    \
-		"    neg x0, x0\n"                                 \
-		"1:  ldr x30, [sp]\n"                              \
-		"    add sp, sp, #32\n"                            \
-		"    ret\n");
+__asm__(
+    "_syscall_sched_yield:\n"
+    "    sub sp, sp, #16\n"
+    "    str x30, [sp]\n"
+    "    mov x16, #158\n"
+    "    orr x16, x16, #0x2000000\n"
+    "    svc #0x80\n"
+    "    b.cc 1f\n"
+    "    neg x0, x0\n"
+    "1:  ldr x30, [sp]\n"
+    "    add sp, sp, #16\n"
+    "    ret\n");
 #endif /* __APPLE__ */
-
-DECLARE_SYSCALL(int, sched_yield, 24, 158, void)
 
 int sched_yield(void) {
 	int v = syscall_sched_yield();
@@ -52,13 +32,3 @@ int sched_yield(void) {
 	return v;
 }
 
-DECLARE_SYSCALL(ssize_t, write, 1, 4, int, void *buf, int)
-
-int write(int fd, void *buf, int length) {
-	int v = syscall_write(fd, buf, length);
-	if (v < 0) {
-		err = -v;
-		return -1;
-	}
-	return v;
-}
