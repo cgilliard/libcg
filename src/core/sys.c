4178019522d4f26a0effe17a21d76a063aa08334
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-20 20:38:12 -0700

    remove unused code

--- src/core/sys.c
+++ src/core/sys.c
@@ -32,29 +32,6 @@
 		"    ret\n");
 #endif
 
-/*
-int syscall_sched_yield(void);
-#ifdef __linux__
-__asm__(
-    "_syscall_sched_yield:\n"
-    "    movq $24, %rax\n"
-    "    syscall\n"
-    "    ret\n");
-#elif defined(__APPLE__)
-__asm__(
-    "_syscall_sched_yield:\n"
-    "    sub sp, sp, #16\n"
-    "    str x30, [sp]\n"
-    "    mov x16, #158\n"
-    "    orr x16, x16, #0x2000000\n"
-    "    svc #0x80\n"
-    "    b.cc 1f\n"
-    "    neg x0, x0\n"
-    "1:  ldr x30, [sp]\n"
-    "    add sp, sp, #16\n"
-    "    ret\n");
-#endif *//* __APPLE__ */
-
 DECLARE_SYSCALL(int, sched_yield, 24, 158, void)
 
 int sched_yield(void) {
@@ -65,29 +42,6 @@ int sched_yield(void) {
 	}
 	return v;
 }
-
-/*
-ssize_t syscall_write(int fd, void *buf, size_t length);
-#ifdef __linux__
-__asm__(
-    "_syscall_write:\n"
-    "    movq $1, %rax\n"
-    "    syscall\n"
-    "    ret\n");
-#elif defined(__APPLE__)
-__asm__(
-    "_syscall_write:\n"
-    "    sub sp, sp, #16\n"
-    "    str x30, [sp]\n"
-    "    mov x16, #4\n"
-    "    orr x16, x16, #0x2000000\n"
-    "    svc #0x80\n"
-    "    b.cc 1f\n"
-    "    neg x0, x0\n"
-    "1:  ldr x30, [sp]\n"
-    "    add sp, sp, #16\n"
-    "    ret\n");
-#endif *//* __APPLE__ */
 
 DECLARE_SYSCALL(ssize_t, write, 1, 4, int fd, void *buf, size_t length)
 
