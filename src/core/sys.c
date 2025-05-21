5a6389e5518e84ce3751dd5998427388a362d547
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-20 19:59:21 -0700

    improve macro

--- src/core/sys.c
+++ src/core/sys.c
@@ -1,4 +1,5 @@
 #include <error.h>
+#include <types.h>
 
 #ifdef __linux__
 long syscall_wrapper(long id, ...);
@@ -16,31 +17,45 @@ __asm__(
 #endif
 
 #ifdef __linux__
-#define DECLARE_SYSCALL(name, linux_num, macos_num, ...) \
-	int syscall_##name(__VA_ARGS__) { return syscall_wrapper(linux_num); }
+#define DECLARE_SYSCALL(ret_type, name, linux_num, macos_num, ...) \
+	ret_type syscall_##name(__VA_ARGS__) {                     \
+		return syscall_wrapper(linux_num);                 \
+	}
 #elif defined(__APPLE__)
-#define DECLARE_SYSCALL(name, linux_num, macos_num, ...) \
-	int syscall_##name(__VA_ARGS__);                 \
-	__asm__(".global _syscall_" #name                \
-		"\n"                                     \
-		"_syscall_" #name                        \
-		":\n"                                    \
-		"    sub sp, sp, #32\n"                  \
-		"    str x30, [sp]\n"                    \
-		"    mov x16, #" #macos_num              \
-		"\n"                                     \
-		"    orr x16, x16, #0x2000000\n"         \
-		"    svc #0x80\n"                        \
-		"    b.cc 1f\n"                          \
-		"    neg x0, x0\n"                       \
-		"1:  ldr x30, [sp]\n"                    \
-		"    add sp, sp, #32\n"                  \
+#define DECLARE_SYSCALL(ret_type, name, linux_num, macos_num, ...) \
+	ret_type syscall_##name(__VA_ARGS__);                      \
+	__asm__(".global _syscall_" #name                          \
+		"\n"                                               \
+		"_syscall_" #name                                  \
+		":\n"                                              \
+		"    sub sp, sp, #32\n"                            \
+		"    str x30, [sp]\n"                              \
+		"    mov x16, #" #macos_num                        \
+		"\n"                                               \
+		"    orr x16, x16, #0x2000000\n"                   \
+		"    svc #0x80\n"                                  \
+		"    b.cc 1f\n"                                    \
+		"    neg x0, x0\n"                                 \
+		"1:  ldr x30, [sp]\n"                              \
+		"    add sp, sp, #32\n"                            \
 		"    ret\n");
 #endif /* __APPLE__ */
 
-DECLARE_SYSCALL(sched_yield, 24, 158, void)
+DECLARE_SYSCALL(int, sched_yield, 24, 158, void)
+
 int sched_yield(void) {
 	int v = syscall_sched_yield();
+	if (v < 0) {
+		err = -v;
+		return -1;
+	}
+	return v;
+}
+
+DECLARE_SYSCALL(ssize_t, write, 1, 4, int, void *buf, int)
+
+int write(int fd, void *buf, int length) {
+	int v = syscall_write(fd, buf, length);
 	if (v < 0) {
 		err = -v;
 		return -1;
---
+++ src/include/types.h
@@ -0,0 +1,133 @@
+#ifndef _TYPES_H__
+#define _TYPES_H__
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#ifndef UINT8_MIN
+#define UINT8_MIN 0x0
+#endif
+
+#ifndef UINT16_MIN
+#define UINT16_MIN 0x0
+#endif
+
+#ifndef UINT32_MIN
+#define UINT32_MIN 0x0
+#endif
+
+#ifndef UINT64_MIN
+#define UINT64_MIN 0x0
+#endif
+
+#ifndef UINT128_MIN
+#define UINT128_MIN 0x0
+#endif
+
+#ifndef UINT8_MAX
+#define UINT8_MAX 0xFF
+#endif
+
+#ifndef UINT16_MAX
+#define UINT16_MAX 0xFFFF
+#endif
+
+#ifndef UINT32_MAX
+#define UINT32_MAX 0xFFFFFFFF
+#endif
+
+#ifndef UINT64_MAX
+#define UINT64_MAX 0xFFFFFFFFFFFFFFFF
+#endif
+
+#ifndef UINT128_MAX
+#define UINT128_MAX ((__uint128_t) - 1)
+#endif
+
+#ifndef INT8_MIN
+#define INT8_MIN (-0x7F - 1)
+#endif
+
+#ifndef INT16_MIN
+#define INT16_MIN (-0x7FFF - 1)
+#endif
+
+#ifndef INT32_MIN
+#define INT32_MIN (-0x7FFFFFFF - 1)
+#endif
+
+#ifndef INT64_MIN
+#define INT64_MIN (-0x7FFFFFFFFFFFFFFF - 1)
+#endif
+
+#ifndef INT128_MIN
+#define INT128_MIN (-(((__int128_t)1 << 127) - 1) - 1)
+#endif
+
+#ifndef INT8_MAX
+#define INT8_MAX 0x7F
+#endif
+
+#ifndef INT16_MAX
+#define INT16_MAX 0x7FFF
+#endif
+
+#ifndef INT32_MAX
+#define INT32_MAX 0x7FFFFFFF
+#endif
+
+#ifndef INT64_MAX
+#define INT64_MAX 0x7FFFFFFFFFFFFFFF
+#endif
+
+#ifndef INT128_MAX
+#define INT128_MAX (((__int128_t)1 << 127) - 1)
+#endif
+
+#ifndef bool
+#define bool byte
+#endif
+
+#ifndef false
+#define false ((bool)0)
+#endif
+
+#ifndef true
+#define true ((bool)1)
+#endif
+
+typedef __int128_t int128_t;
+typedef __uint128_t uint128_t;
+typedef unsigned char byte;
+
+/* Begin stdint defs */
+typedef signed char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+
+#ifdef __linux__
+typedef long int64_t;
+typedef unsigned long uint64_t;
+#elif defined(__APPLE__)
+typedef long int64_t;
+typedef unsigned long uint64_t;
+#else
+#error Unsupported platform. Supported platforms: __linux__ or __APPLE__
+#endif
+
+/* to use stdint, comment out above and uncomment below */
+/* #include <stdint.h> */
+
+/* Begin sys/types defs */
+typedef unsigned long size_t;
+typedef long ssize_t;
+
+/* to use sys/types, comment out above and uncomment below */
+/* #include <sys/types.h> */
+
+#endif /* _TYPES_H__ */
+
